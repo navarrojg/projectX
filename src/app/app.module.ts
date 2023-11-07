827d59feb9cd86206d26c26d2c678794ac1fe35f
bartek <taras_basket@wp.pl>
2023-11-07 22:21:42 +0100

    working on cards- angularPart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,17 @@ import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { PetsListComponent } from './pets/pets-list/pets-list.component';
 import { PetCreateComponent } from './pets/pet-create/pet-create.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, PetsListComponent, PetCreateComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PetsListComponent,
+    PetCreateComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -21,6 +27,7 @@ import { PetCreateComponent } from './pets/pet-create/pet-create.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
