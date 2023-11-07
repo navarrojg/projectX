5c3d1b17d1716a72c2bf83e9fcd0d7032334d5d4
bartek <taras_basket@wp.pl>
2023-11-07 22:55:30 +0100

    styling cards, working on petService, petListComponent

--- src/app/pets/pets-list/pets-list.component.ts
+++ src/app/pets/pets-list/pets-list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PetsService } from '../pets.service';
+import { Pet } from '../pet.model';
 
 @Component({
   selector: 'app-pets-list',
   templateUrl: './pets-list.component.html',
-  styleUrls: ['./pets-list.component.css']
+  styleUrls: ['./pets-list.component.css'],
 })
 export class PetsListComponent {
-
+  pets: Pet[] = [
+    { name: 'Max', sex: 'M', age: 12, breed: 'german shepard' },
+    { name: 'Alex', sex: 'F', age: 5, breed: 'german shepard' },
+    { name: 'Misty', sex: 'F', age: 2, breed: 'german shepard' },
+  ];
 }
--- src/app/pets/pets.service.ts
+++ src/app/pets/pets.service.ts
@@ -1,5 +1,12 @@
 import { Pet } from './pet.model';
 
 export class PetsService {
-  list0fPets: Pet[] = [];
+  private list0fPets: Pet[] = [
+    { name: 'Max', sex: 'M', age: 12, breed: 'german shepard' },
+    { name: 'Alex', sex: 'F', age: 5, breed: 'german shepard' },
+    { name: 'Misty', sex: 'F', age: 2, breed: 'german shepard' },
+  ];
+  getPets() {
+    return this.list0fPets;
+  }
 }
