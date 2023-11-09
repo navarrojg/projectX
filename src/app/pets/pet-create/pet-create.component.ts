e8bd3242c5a9928dfcf019ee2c988c4c0a1a39a8
bartek <taras_basket@wp.pl>
2023-11-09 22:11:38 +0100

    fixing petCreateForm

--- src/app/pets/pet-create/pet-create.component.ts
+++ src/app/pets/pet-create/pet-create.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PetsService } from '../pets.service';
 
-
 @Component({
   selector: 'app-pet-create',
   templateUrl: './pet-create.component.html',
@@ -10,17 +9,29 @@ import { PetsService } from '../pets.service';
 })
 export class PetCreateComponent implements OnInit, OnDestroy {
   form: FormGroup;
-
+  enteredTitle = '';
+  enteredContent = '';
 
   constructor(petsService: PetsService) {}
+
   ngOnInit() {
     this.form = new FormGroup({
       name: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(1)],
       }),
+      sex: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      age: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      breed: new FormControl(null, {
+        validators: [Validators.required],
+      }),
     });
   }
   onSavePost() {}
+
   onImagePicked() {}
 
   ngOnDestroy(): void {}
