6889911cdd17bfe8cf29b233375e0f9f87b8beed
bartek <taras_basket@wp.pl>
2023-11-10 21:21:11 +0100

    adding new methonds to petsService

--- src/app/pets/pet-create/pet-create.component.ts
+++ src/app/pets/pet-create/pet-create.component.ts
@@ -11,26 +11,32 @@ export class PetCreateComponent implements OnInit, OnDestroy {
   form: FormGroup;
   enteredTitle = '';
   enteredContent = '';
+  private mode = 'create';
 
   constructor(petsService: PetsService) {}
 
-  ngOnInit() {
-    this.form = new FormGroup({
-      name: new FormControl(null, {
-        validators: [Validators.required, Validators.minLength(1)],
-      }),
-      sex: new FormControl(null, {
-        validators: [Validators.required],
-      }),
-      age: new FormControl(null, {
-        validators: [Validators.required],
-      }),
-      breed: new FormControl(null, {
-        validators: [Validators.required],
-      }),
-    });
+  ngOnInit() {}
+  onSavePost() {
+    if (this.form.invalid) {
+      return;
+    }
+    if (this.mode === 'create') {
+      this.form = new FormGroup({
+        name: new FormControl(null, {
+          validators: [Validators.required, Validators.minLength(1)],
+        }),
+        sex: new FormControl(null, {
+          validators: [Validators.required],
+        }),
+        age: new FormControl(null, {
+          validators: [Validators.required],
+        }),
+        breed: new FormControl(null, {
+          validators: [Validators.required],
+        }),
+      });
+    }
   }
-  onSavePost() {}
 
   onImagePicked() {}
 
--- src/app/pets/pets.service.ts
+++ src/app/pets/pets.service.ts
@@ -3,16 +3,25 @@ import { Pet } from './pet.model';
 import { Subject } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class PetsService {
-  private list0fPets: Pet[] = [
+  private pets: Pet[] = [
     { name: 'Max', sex: 'M', age: 12, breed: 'german shepard' },
     { name: 'Alex', sex: 'F', age: 5, breed: 'german shepard' },
     { name: 'Misty', sex: 'F', age: 2, breed: 'german shepard' },
   ];
+
   private petsUpdate = new Subject<Pet[]>();
 
   getPets() {
-    return [...this.list0fPets];
+    return [...this.pets];
   }
 
-  addPost() {}
+  getPetUpdateListener() {
+    return this.petsUpdate.asObservable();
+  }
+
+  addPet(name: string, sex: string, age: number, breed: string) {
+    const pet: Pet = { name: name, sex: sex, age: age, breed: breed };
+    this.pets.push(pet);
+    this.petsUpdate.next([...this.pets]);
+  }
 }
