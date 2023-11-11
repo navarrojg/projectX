a337079163d2da016a201137cdce885212c11941
bartek <taras_basket@wp.pl>
2023-11-11 17:56:24 +0100

    setting sub in petsListComponent

--- src/app/pets/pets-list/pets-list.component.ts
+++ src/app/pets/pets-list/pets-list.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PetsService } from '../pets.service';
 import { Pet } from '../pet.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pets-list',
   templateUrl: './pets-list.component.html',
   styleUrls: ['./pets-list.component.css'],
 })
-export class PetsListComponent implements OnInit {
+export class PetsListComponent implements OnInit, OnDestroy {
   pets: Pet[] = [];
+  private petSub: Subscription;
 
   constructor(private petsService: PetsService) {}
 
   ngOnInit() {
     this.pets = this.petsService.getPets();
+    this.petSub = this.petsService
+      .getPetUpdateListener()
+      .subscribe((pets: Pet[]) => {
+        this.pets = pets;
+      });
+  }
+
+  ngOnDestroy() {
+    this.petSub.unsubscribe();
   }
 }
