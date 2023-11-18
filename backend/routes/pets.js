cc2d67fa674653f5c459fabb253dfae3fd88ed61
bartek <taras_basket@wp.pl>
2023-11-18 14:06:10 +0100

    creating middleware to be able to upload images

--- backend/routes/pets.js
+++ backend/routes/pets.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
+const extractFile = require("../middleware/file");
+
 const PetController = require("../controllers/pet");
 const router = express.Router();
 
-router.post("", PetController.createPet);
-router.get("", PetController.getPets);
+router.post("", extractFile, PetController.createPet);
+router.get("", extractFile, PetController.getPets);
 router.get("/:id", PetController.getPet);
 router.delete("/:id", PetController.deletePet);
 router.put("/:id", PetController.updatePet);
