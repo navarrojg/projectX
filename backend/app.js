fceaaffcd6045f7dba8b910e945dce26e040eb34
bartek <taras_basket@wp.pl>
2023-11-18 15:28:44 +0100

    working on fetching images on frontend and updating petsListComponent

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,10 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  "/images/pets-images",
+  express.static(path.join("backend/images/pets-images"))
+);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
