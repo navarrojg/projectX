a6ef6f09ce2d99ceed3a5a5b51089cf97bde160a
bartek <taras_basket@wp.pl>
2023-11-25 17:50:18 +0100

    creating createUserMethond in the backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,6 +2,23 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
-exports.createUser = (req, res, next) => {};
+exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 11).then((hash) => {
+    const user = new User({ email: req.body.email, password: hash });
+  });
+  user
+    .save()
+    .then((createdUser) => {
+      res.status(201).json({
+        message: "User created!",
+        user: createdUser,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
 exports.userLogin = (req, res, next) => {};
