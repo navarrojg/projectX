aec4d54ef725074e912592681c6860da69837b2e
bartek <taras_basket@wp.pl>
2023-11-26 22:13:56 +0100

    adding authGuard to code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PetCreateComponent } from './pets/pet-create/pet-create.component';
 import { PetDetailsComponent } from './pets/pet-details/pet-details.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/pets', pathMatch: 'full' },
@@ -14,8 +15,8 @@ const routes: Routes = [
     // children: [{ path: ':id', component: PetDetailsComponent }],
   },
   { path: 'pets/:id', component: PetDetailsComponent },
-  { path: 'create', component: PetCreateComponent },
-  { path: 'edit/:id', component: PetCreateComponent },
+  { path: 'create', component: PetCreateComponent, canActivate: [AuthGuard] },
+  { path: 'edit/:id', component: PetCreateComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
@@ -23,5 +24,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard],
 })
 export class AppRoutingModule {}
