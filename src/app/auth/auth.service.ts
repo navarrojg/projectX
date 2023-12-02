5de868bbd9e5403f16ab2651bc273828d364e4e1
bartek <taras_basket@wp.pl>
2023-12-02 13:21:44 +0100

    adding new methods autAuthUser, setAuthTimer

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -48,9 +48,7 @@ export class AuthService {
         this.token = token;
         if (token) {
           const expiresInDuration = response.expiresIn;
-          this.tokenTimer = setTimeout(() => {
-            this.logout();
-          }, expiresInDuration * 1000);
+          this.setAuthTimer(expiresInDuration);
           this.authStatusListener.next(true);
           this.isAuthenticated = true;
           const now = new Date();
@@ -61,6 +59,18 @@ export class AuthService {
           this.router.navigate(['/']);
         }
       });
+  }
+
+  autoAuthUser() {
+    const authInformation = this.getAuthData();
+    const now = new Date();
+    const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+    if (expiresIn > 0) {
+      this.token = authInformation.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn);
+      this.authStatusListener.next(true);
+    }
   }
 
   logout() {
@@ -77,8 +87,27 @@ export class AuthService {
     localStorage.setItem('expiration', expirationDate.toISOString());
   }
 
+  private setAuthTimer(duration: number) {
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000);
+  }
+
   private clearAuthDate() {
     localStorage.removeItem('token');
     localStorage.removeItem('expiration');
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expiration');
+
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token: token,
+      expirationDate: new Date(expirationDate),
+    };
   }
 }
