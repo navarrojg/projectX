e55c1cafb59957fafaf5f359e8f4150b650c4e0b
bartek <taras_basket@wp.pl>
2023-12-03 20:56:08 +0100

    creator added to pet model on the backend

--- backend/models/pet.js
+++ backend/models/pet.js
@@ -6,6 +6,11 @@ const petSchema = mongoose.Schema({
   age: { type: Number, required: true },
   breed: { type: String, required: true },
   imagePath: { type: String, required: true },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Pet", petSchema);
