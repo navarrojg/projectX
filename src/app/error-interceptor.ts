3d526a9dcccc74282a80cd1555aca65b8374d511
bartek <taras_basket@wp.pl>
2023-12-07 22:23:23 +0100

    styling and setting errorDialog

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -19,7 +19,11 @@ export class ErrorInterceptor implements HttpInterceptor {
       catchError((error: HttpErrorResponse) => {
         // console.log(error);
         // alert(error.error.error.message);
-        this.dialog.open(ErrorComponent);
+        let errorMessage = 'An unkown error occurred!';
+        if (error.error.message) {
+          errorMessage = error.error.message;
+        }
+        this.dialog.open(ErrorComponent, { data: { message: errorMessage } });
         return throwError(error);
       })
     );
--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-error',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./error.component.css'],
 })
 export class ErrorComponent {
-  message='An unkown error occured.';
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string }) {}
 }
