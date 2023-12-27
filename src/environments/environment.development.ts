c2962e18cdcae64b7f92a51fa4ae36338a85c8d0
bartek <taras_basket@wp.pl>
2023-12-27 21:36:06 +0100

    changing html code to select for breed/type

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:3000/api',
-  apiUrl: 'https://addpetapi-9f1ddd456dda.herokuapp.com/api',
+  apiUrl: 'http://localhost:3000/api',
+  // apiUrl: 'https://addpetapi-9f1ddd456dda.herokuapp.com/api',
 };
